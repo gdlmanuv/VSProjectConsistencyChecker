592e52482c53231d130805b25e80ebcc95c89c78
Manuel Valle <manuelvalle1984@hotmail.com>
2022-02-18 11:26:39 -0600

    Initial code completed

---
+++ src/VSProjectConsistencyChecker.Common/Resolvers/ProjectConsistencyResolver.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace VSProjectConsistencyChecker.Common.Resolvers
+{
+    public class ProjectConsistencyResolver
+    {
+        public string GenerateConsistencyAssesmentReport(string location, ref int totalScannedFiles, ref string outputFile, string[] extensions = null)
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine("Project,Configuration,Platform,Target,Output, Platforms Match, Expected output, Outputs Match");
+
+            if (extensions == null)
+            {
+                extensions = new string[] { "*.vbproj", "*.csproj" };
+            }
+
+            foreach (var extension in extensions)
+            {
+                GenerateConsistencyAssesmentReportInternal(location, extension, builder, ref totalScannedFiles);
+            }
+
+            outputFile = $"Output_{DateTime.Now.ToString("yyyyMMddhhmmss")}.csv";
+
+            File.WriteAllText(outputFile, builder.ToString().Replace("|", ","));
+            
+            TryOpenFile(outputFile);
+
+            return builder.ToString().Replace("|", ",");
+        }
+
+        private static void TryOpenFile(string outputFile)
+        {
+            try
+            {
+                Process.Start(outputFile);
+            }
+            catch
+            {
+                // Handled exception to avoid breaking the application when output file cannot directly be opened
+                // from here.
+            }
+        }
+
+        private void GenerateConsistencyAssesmentReportInternal(string location, string extension, StringBuilder builder, ref int totalFiles)
+        {
+            string fileName;
+            string configurationAndPlatform;
+            string platformTarget;
+            string outputPath;
+            string platform;
+            string expectedOutput;
+            string configuration;
+
+            foreach (var f in Directory.GetFiles(location, extension, SearchOption.AllDirectories))
+            {
+                var fileInfo = new FileInfo(f);
+
+                using (var reader = new StreamReader(fileInfo.FullName))
+                {
+                    XDocument xDocument = XDocument.Load(reader);
+
+                    var propertyGroupElements = xDocument.Root.Elements().Where(e => e.Name.LocalName == "PropertyGroup").ToList();
+
+                    foreach (var propertyGroupElement in propertyGroupElements)
+                    {
+                        fileName = "";
+                        configurationAndPlatform = "";
+                        platformTarget = "";
+                        outputPath = "";
+                        platform = "";
+                        expectedOutput = "";
+                        configuration = "";
+
+                        if (propertyGroupElement.HasAttributes && propertyGroupElement.Attributes().Any(a => a.Name.LocalName.Equals("Condition")))
+                        {
+                            fileName = fileInfo.Name;                            
+                            configurationAndPlatform = propertyGroupElement.Attributes().First(
+                                    a => a.Name.LocalName.Equals("Condition")).Value.Replace("'$(Configuration)|$(Platform)' == ", "").Replace("'", "").Trim();
+                            builder.Append(fileName).Append("|").Append(configurationAndPlatform);
+
+                            if (propertyGroupElement.Elements().Any(e => e.Name.LocalName == "PlatformTarget"))
+                            {
+                                platformTarget = propertyGroupElement.Elements().First(e => e.Name.LocalName == "PlatformTarget").Value;
+                                builder.Append("|").Append(platformTarget);
+                            }
+                            else
+                            {
+                                builder.Append("|");
+                            }
+
+                            if (propertyGroupElement.Elements().Any(e => e.Name.LocalName == "OutputPath"))
+                            {
+                                outputPath = propertyGroupElement.Elements().First(e => e.Name.LocalName == "OutputPath").Value;
+                                builder.Append("|").Append(outputPath);
+                            }
+
+                            // Verify whether platform matches with platform targetx
+                            configuration = configurationAndPlatform.Split('|').First();
+                            platform = configurationAndPlatform.Split('|').Last();
+
+                            if ((string.IsNullOrWhiteSpace(platformTarget) && platform.Equals("AnyCPU")) || platformTarget.Equals(platform))
+                            {
+                                builder.Append("|").Append("CORRECT");
+                            }
+                            else
+                            {
+                                builder.Append("|").Append("INCORRECT");
+                            }
+
+                            // Verify whether output matches with expected output
+
+                            expectedOutput = $@"bin\{(platform.Equals("AnyCPU") ? "" : $@"{platform}\")}{configuration}\";
+                            builder.Append("|").Append(expectedOutput);
+
+                            if (expectedOutput.Equals(outputPath))
+                            {
+                                builder.Append("|").Append("CORRECT");
+                            }
+                            else
+                            {
+                                builder.Append("|").Append("INCORRECT");
+                            }
+
+                            builder.AppendLine();
+                        }
+                    }
+                }
+
+                totalFiles++;
+            }
+        }
+    }
+}
---
+++ src/VSProjectConsistencyChecker/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using VSProjectConsistencyChecker.Common.Resolvers;
+
+namespace VSProjectConsistencyChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int totalScannedFiles = 0;
+                string outputFile = "";
+                ProjectConsistencyResolver resolver = new ProjectConsistencyResolver();
+                string output = resolver.GenerateConsistencyAssesmentReport(args[1], ref totalScannedFiles, ref outputFile);
+                Console.WriteLine(output.Replace(",", " "));
+                Console.WriteLine($"Total scanned projects: {totalScannedFiles}");
+                Console.WriteLine($"Assesment report: {Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), outputFile)}");
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex);
+                Console.WriteLine();
+                Console.WriteLine("Usage:");
+                Console.WriteLine("VSProjectConsistencyChecker -path <The path where projects are located>");
+            }
+            finally
+            {
+                Console.WriteLine("Press any key to close.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
